d24db09f19aa0757e99cc9db11fac4280135ce81
harshitvarshney1 <72073964+harshitvarshney1@users.noreply.github.com>
2021-05-13 16:34:07 +0530

    Product-Catalog with add, delete, list, search operations

---
+++ ProductCatalog1/CategoryLiberary/CFileoperation.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using System.IO;
+using CategoryLibrary.Entities;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace ProductCatalog1
+{
+    public class CFileoperation
+    {
+        public static void fileRead()
+        {
+            //string dirPath = @"C:/Users/s s infotech/source/repos/ProductCatalog1";
+            string filePath = @"C:/Users/s s infotech/source/repos/ProductCatalog1/Category.csv";
+            using (StreamReader input = File.OpenText(filePath))
+            using (CsvHelper.CsvReader csvreader = new CsvHelper.CsvReader(input, System.Globalization.CultureInfo.CreateSpecificCulture("en-SI")))
+            {
+                IEnumerable<Category> category = csvreader.GetRecords<Category>();
+                Console.WriteLine();
+                Console.WriteLine("Id\t Name\t\t ShortCode\tDescription");
+                foreach (Category record in category)
+                {
+                    Console.WriteLine(record.C_ID+ "\t" + record.Name + "\t\t" + record.ShortCode+ "\t" + record.Description);                
+                }
+            }
+        }
+        public static void fileWrite(Category category)
+        {
+            List<Category> cdata = new List<Category>();
+            string filePath = @"C:/Users/s s infotech/source/repos/ProductCatalog1/Category.csv";
+            //string lastLine = File.ReadLines(filePath).Last();
+            //int s = lastLine[0] - '0';
+            //Console.WriteLine(Convert.ToInt32(s));
+            //Console.WriteLine(lastLine);
+            
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture);
+            config.HasHeaderRecord = false;
+            using (StreamWriter input = new StreamWriter(filePath, true))
+            using (CsvHelper.CsvWriter csvwriter = new CsvHelper.CsvWriter(input, config))
+            {
+                cdata.Add(category);
+                csvwriter.WriteRecords(cdata);
+                input.Flush();
+            }
+        }
+
+        public static void rowDelete(int id)
+        {
+            List<Category> records;
+            string filePath = @"C:/Users/s s infotech/source/repos/ProductCatalog1/Category.csv";
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                records = csv.GetRecords<Category>().ToList();
+                for (int i = 0; i < records.Count; ++i)
+                {
+                    if (records[i].C_ID == id)
+                    {
+                        records.RemoveAt(i);
+                    }
+                }
+            }
+            using (var writer = new StreamWriter(filePath))
+            using (var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csvWriter.WriteRecords(records);
+            }
+        }
+
+        public static void rowSearch(string name)
+        {
+            string filePath = @"C:/Users/s s infotech/source/repos/ProductCatalog1/Category.csv";
+            var strLines = File.ReadLines(filePath);
+            foreach (var line in strLines)
+            {
+
+                if (line.Split(',')[1].Equals(name))
+                {
+                    Console.WriteLine(line);
+                    return;
+                }
+
+            }
+        }
+    }
+}
---
+++ ProductCatalog1/CategoryLiberary/Menu/CategoryMenu.cs
@@ -0,0 +1,58 @@
+﻿using CategoryLibrary.Entities;
+using ProductCatalog1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CategoryLibrary
+{
+    public class CategoryMenu
+    {
+        public void showCategoryMenu()
+        {
+            Console.WriteLine("Enter Choice for Category");
+            Console.WriteLine("\ta- Enter a Category");
+            Console.WriteLine("\tb- List all Categories");
+            Console.WriteLine("\tc- Delete a Category (Enter Short Code or ID to delete)");
+            Console.WriteLine("\td- Search a Category (By Id, Name, Short Code)");
+            string choice = Console.ReadLine();
+            switch (choice.ToLower())
+            {
+                case "a":
+                    string filePath = @"C:/Users/s s infotech/source/repos/ProductCatalog1/Category.csv";
+                    string lastLine = File.ReadLines(filePath).Last();
+                    int lastrowid = lastLine[0] - '0';
+                    Category.cId = lastrowid;
+                    Console.WriteLine("Enter Category Name");
+                    string categoryName = Console.ReadLine();
+                    Console.WriteLine("Enter Short Code");
+                    string shortCode = Console.ReadLine();
+                    Console.WriteLine("Enter Description");
+                    string description = Console.ReadLine();
+                    Category category = new Category { C_ID = Category.cId, Name = categoryName, ShortCode = shortCode, Description = description };
+                    CFileoperation.fileWrite(category);
+                    break;
+
+                case "b":
+                    CFileoperation.fileRead();
+                    break;
+                case "c":
+                    Console.WriteLine("Enter Id to delete");
+                    int delete = Convert.ToInt32(Console.ReadLine());
+                    CFileoperation.rowDelete(delete);
+                    break;
+                case "d":
+                    Console.WriteLine("Enter Name to search");
+                    string search = Console.ReadLine();
+                    CFileoperation.rowSearch(search);
+                    break;
+                default:
+                    Console.WriteLine("Please Enter Correct Choice");
+                    break;
+            }
+        }
+    }
+}
